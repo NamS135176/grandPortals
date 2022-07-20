bc04bb32c657bda91534ec094833197849a906da
namnt <namnt@vn-sis.com>
2022-07-20 14:18:16 +0700

    add

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,33 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "userPoolGroups": {
-            "agentGrandsGroupRole": "string",
-            "agentNormalGroupRole": "string",
-            "supportGroupRole": "string"
-        },
-        "grandsPortal": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
     "function": {
         "grandsPortalPostConfirmation": {
             "Name": "string",
             "Arn": "string",
             "LambdaExecutionRole": "string",
             "Region": "string"
-        }
-    },
-    "api": {
-        "grandsportal": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
         }
     },
     "hosting": {
