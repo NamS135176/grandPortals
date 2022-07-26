43f293126ecbd6179a346bab156354bf708168e5
Inna K <i.kostiuk@benjamin.co.jp>
2022-07-26 16:25:49 +0800

    add error page GRANDS_PORTAL_SITE-51 404error

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,93 +1,58 @@
-import { useEffect } from 'react';
-import NextLink from 'next/link';
+import {useEffect} from 'react';
 import Head from 'next/head';
-import { Box, Button, Container, Typography, useMediaQuery } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import { gtm } from '../lib/gtm';
+import {Box, Container, Typography, useMediaQuery} from '@mui/material';
+import {useTheme} from '@mui/material/styles';
+import {gtm} from '../lib/gtm';
 
 const NotFound = () => {
-  const theme = useTheme();
-  const mobileDevice = useMediaQuery(theme.breakpoints.down('sm'));
+	const theme = useTheme();
+	const mobileDevice = useMediaQuery(theme.breakpoints.down('sm'));
 
-  useEffect(() => {
-    gtm.push({ event: 'page_view' });
-  }, []);
+	useEffect(() => {
+		gtm.push({event: 'page_view'});
+	}, []);
 
-  return (
-    <>
-      <Head>
-        <title>
-          Error: Not Found | Material Kit Pro
-        </title>
-      </Head>
-      <Box
-        component="main"
-        sx={{
-          alignItems: 'center',
-          backgroundColor: 'background.paper',
-          display: 'flex',
-          flexGrow: 1,
-          py: '80px'
-        }}
-      >
-        <Container maxWidth="lg">
-          <Typography
-            align="center"
-            variant={mobileDevice ? 'h4' : 'h1'}
-          >
-            404: The page you are looking for isn’t here
-          </Typography>
-          <Typography
-            align="center"
-            color="textSecondary"
-            sx={{ mt: 0.5 }}
-            variant="subtitle2"
-          >
-            You either tried some shady route or you
-            came here by mistake. Whichever it is, try using the
-            navigation.
-          </Typography>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              mt: 6
-            }}
-          >
-            <Box
-              alt="Under development"
-              component="img"
-              src={`/images/error/error404_${theme.palette.mode}.svg`}
-              sx={{
-                height: 'auto',
-                maxWidth: '100%',
-                width: 400
-              }}
-            />
-          </Box>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              mt: 6
-            }}
-          >
-            <NextLink
-              href="/dashboard"
-              passHref
-            >
-              <Button
-                component="a"
-                variant="outlined"
-              >
-                Back to Dashboard
-              </Button>
-            </NextLink>
-          </Box>
-        </Container>
-      </Box>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>ページが見つかりませんでした。</title>
+			</Head>
+			<Box
+				sx={{
+					alignItems: 'center',
+					display: 'flex',
+					minHeight: '100%',
+					px: 3,
+					py: '80px',
+				}}
+			>
+				<Container maxWidth="lg">
+					<Typography
+						align="center"
+						color="textPrimary"
+						variant={mobileDevice ? 'h3' : 'h1'}
+					>
+						404
+					</Typography>
+					<Typography
+						align="center"
+						color="textPrimary"
+						variant={mobileDevice ? 'h4' : 'h3'}
+					>
+						ページが見つかりませんでした。
+					</Typography>
+					<Typography
+						align="center"
+						color="textSecondary"
+						sx={{mt: 0.5}}
+						variant="subtitle2"
+					>
+						リクエストされたページは見つかりませんでした。
+					</Typography>
+				</Container>
+			</Box>
+		</>
+	);
 };
 
 export default NotFound;
