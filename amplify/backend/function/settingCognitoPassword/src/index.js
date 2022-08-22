56c9ec78a4db5751652acfb15904f929beff430a
Nam Nguyen <namnt1706@gmail.com>
2022-08-22 15:37:59 +0700

    add function

--- amplify/backend/function/settingCognitoPassword/src/index.js
+++ amplify/backend/function/settingCognitoPassword/src/index.js
@@ -1,6 +1,7 @@
 /* Amplify Params - DO NOT EDIT
 	API_GRANDSPORTAL_GRAPHQLAPIENDPOINTOUTPUT
 	API_GRANDSPORTAL_GRAPHQLAPIIDOUTPUT
+	AUTH_GRANDSPORTAL_USERPOOLID
 	ENV
 	REGION
 Amplify Params - DO NOT EDIT */
@@ -8,15 +9,31 @@ Amplify Params - DO NOT EDIT */
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-};
+ const AWS = require("aws-sdk");
+ const UserPoolId = process.env.AUTH_GRANDSPORTAL_USERPOOLID
+ const cognito = new AWS.CognitoIdentityServiceProvider();
+ 
+ exports.handler = async (event) => {
+     try {
+         const email = event['arguments']['input']['email'];
+         const password = event['arguments']['input']['password'];
+         await cognito.adminSetUserPassword({
+             UserPoolId,
+             Username: email,
+             Password: password,
+             Permanent: true
+         }).promise();
+         const response = {
+             statusCode: 200,
+             body: JSON.stringify('success'),
+         };
+         return response;
+     } catch (err) {
+         const response = {
+             statusCode: 500,
+             body: JSON.stringify(err)
+         }
+         return response;
+     }
+ };
+ 
