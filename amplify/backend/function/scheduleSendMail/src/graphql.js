4727c7a46a2557ff389f0de165e07efc7589afeb
Nam Nguyen <namnt1706@gmail.com>
2022-09-08 15:19:51 +0700

    add cronjob

--- amplify/backend/function/scheduleSendMail/src/graphql.js
+++ amplify/backend/function/scheduleSendMail/src/graphql.js
@@ -33,3 +33,52 @@ query ListInformation(
   }
 }
 `;
+
+module.exports.queryInformationListSendByInformationId = /* GraphQL */ `
+query QueryInformationListSendByInformationId(
+  $information_id: ID!
+  $sortDirection: ModelSortDirection
+  $filter: ModelInformationListSendFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  queryInformationListSendByInformationId(
+    information_id: $information_id
+    sortDirection: $sortDirection
+    filter: $filter
+    limit: $limit
+    nextToken: $nextToken
+  ) {
+    items {
+      id
+      information_id
+      information {
+        id
+        subject
+        content
+        scheduled_delivery_date
+        important_info_flag
+        draft_flag
+        delete_flag
+        processed_date
+        sort
+        createdAt
+        updatedAt
+      }
+      user_id
+      email
+      name
+      name_kana
+      withdrawal_flag
+      receive_notification_email_flag
+      last_user_read
+      sort
+      sendgrid_response
+      x_message_id
+      createdAt
+      updatedAt
+    }
+    nextToken
+  }
+}
+`;
--- amplify/backend/function/scheduleSendMail/src/index.js
+++ amplify/backend/function/scheduleSendMail/src/index.js
@@ -31,46 +31,61 @@ const appsyncUrl = process.env.API_GRANDSPORTAL_GRAPHQLAPIENDPOINTOUTPUT;
 const region = process.env.REGION;
 const endpoint = new urlParse(appsyncUrl).hostname.toString();
 const sgMail = require("@sendgrid/mail");
-const {listInformation} = require("./graphql");
+const {
+    listInformation,
+    queryInformationListSendByInformationId,
+} = require("./graphql");
 
 const lambda = new AWS.Lambda({
     region: "ap-northeast-1", //change to your region
 });
 
 const sendResetLink = async (email) => {
-  try {
-      const {Parameters} = await new AWS.SSM()
-          .getParameters({
-              Names: ["SENDGRID_API_KEY"].map(
-                  (secretName) => process.env[secretName]
-              ),
-              WithDecryption: true,
-          })
-          .promise();
-      let sendgridApiKey = Parameters[0].Value;
-      sgMail.setApiKey(sendgridApiKey);
-      const msg = {
-          to: email,
-          from: "no-reply@grands.co.jp",
-          subject: `パスワードリセットのお知らせ`,
-          html: `さん<br /><br />
+    try {
+        const {Parameters} = await new AWS.SSM()
+            .getParameters({
+                Names: ["SENDGRID_API_KEY"].map(
+                    (secretName) => process.env[secretName]
+                ),
+                WithDecryption: true,
+            })
+            .promise();
+        let sendgridApiKey = Parameters[0].Value;
+        sgMail.setApiKey(sendgridApiKey);
+        const msg = {
+            to: email,
+            from: "no-reply@grands.co.jp",
+            subject: `パスワードリセットのお知らせ`,
+            html: `さん<br /><br />
  ===================================<br /> `,
-      };
-      await sgMail.send(msg);
-  } catch (err) {
-      console.log(err);
-      throw err;
-  }
+        };
+        await sgMail.send(msg);
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
 };
 
 const listAllInformation = async (information = [], nextToken = null) => {
     let queryParams = {
         filter: {
             processed_date: {
-              attributeExists: false
+                attributeExists: false,
             },
-            scheduled_delivery_date: {
-                le: moment.utc(new Date()).format("YYYY-MM-DD"),
+            or: [
+                {
+                    scheduled_delivery_date: {
+                        attributeExists: false,
+                    },
+                },
+                {
+                    scheduled_delivery_date: {
+                        le: moment.utc(new Date()).format("YYYY-MM-DD"),
+                    },
+                },
+            ],
+            draft_flag: {
+                eq: 0,
             },
         },
         limit: 1000,
@@ -94,20 +109,87 @@ const listAllInformation = async (information = [], nextToken = null) => {
     }
 };
 
+const listAllInformationSendListByInformationId = async (
+    informationSendList = [],
+    nextToken = null,
+    id
+) => {
+    let queryParams = {
+        information_id: id,
+        filter: {
+            withdrawal_flag: {
+                eq: false,
+            },
+        },
+        limit: 1000,
+    };
+    if (nextToken) {
+        queryParams.nextToken = nextToken;
+    }
+
+    const res = await appSyncRequest(
+        queryInformationListSendByInformationId,
+        "QueryInformationListSendByInformationId",
+        JSON.stringify(queryParams)
+    );
+    console.log(res.data);
+    const {nextToken: nxtToken, items} =
+        res.data?.data?.queryInformationListSendByInformationId;
+    informationSendList = informationSendList.concat(items);
+    if (nxtToken) {
+        return await listAllInformationSendListByInformationId(
+            informationSendList,
+            nxtToken,
+            id
+        );
+    } else {
+        return informationSendList;
+    }
+};
+
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+    // await sendResetLink("namnt@vn-sis.com");
     const res = await listAllInformation();
-    console.log(res);
-    // const response = await lambda.invoke({
-    //   FunctionName: process.env.FUNCTION_PUBLISHINFORMATIONZIPFILE_NAME,
-    //   Payload: JSON.stringify({
-    //    arguments: {
-    //         folder: `public/information/000006/`,
-    //         zipFileName: "archive.zip",
-    //    }
-    //   }) // pass params
-    // }).promise();
-    // console.log(response);
+    if (res.length > 0) {
+        // const response = await Promise.all(res.map(item => {
+        //   return  lambda.invoke({
+        //       FunctionName: process.env.FUNCTION_PUBLISHINFORMATIONZIPFILE_NAME,
+        //       Payload: JSON.stringify({
+        //        arguments: {
+        //             folder: `public/information/${item?.id}/`,
+        //             zipFileName:`${moment().format("YYYYMMDD_HHmmss")}_archive.zip`
+        //        }
+        //       }) // pass params
+        //     }).promise();
+        // }))
+        // console.log(response);
+        const informationSendList = await Promise.all(
+            res.map((item) => {
+                return listAllInformationSendListByInformationId(
+                    [],
+                    null,
+                    item?.id
+                );
+            })
+        );
+        const newInformationSendList = informationSendList.map((it) => {
+            const newIt = it.filter((item) => {
+                if (item.information.important_info_flag == 0) {
+                    return item.receive_notification_email_flag;
+                } else {
+                    return true;
+                }
+            });
+            return newIt;
+        });
+        console.log(res);
+        console.log(newInformationSendList);
+        // const data = newInformationSendList.map(async (item) => {
+        //     return await Promise.all(item.map((it) => sendResetLink(it.email)));
+        // });
+        // console.log(data);
+    }
     return {
         statusCode: 200,
         body: JSON.stringify("Hello from Lambda!"),
