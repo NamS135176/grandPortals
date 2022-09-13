b4c6970a9a3610b2d2515b5c07634083a8eaaeb6
Nam Nguyen <namnt1706@gmail.com>
2022-09-13 09:01:46 +0700

    fix

--- src/pages/cs/information/[id]/index.js
+++ src/pages/cs/information/[id]/index.js
@@ -50,8 +50,8 @@ const CsInformationDetails = () => {
 
 	const [listInformationSend, setListInformationSend] = useState([]);
 	const [files, setFiles] = useState([]);
-	const canEdit = information?.scheduled_delivery_date
-		? moment(information.scheduled_delivery_date).isAfter(moment())
+	const canEdit = information?.processed_date || moment(information?.scheduled_delivery_date).isAfter(moment())
+		? false
 		: true;
 	const draftFlag = useRef(0);
 
