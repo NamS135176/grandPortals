a4a3b5b876be96f25be465cdca4445bb3a2e59af
Nam Nguyen <namnt1706@gmail.com>
2022-09-14 16:17:56 +0700

    fix search time

--- src/hooks/use-information-list.js
+++ src/hooks/use-information-list.js
@@ -10,7 +10,12 @@ import * as R from "ramda";
 import {getBukkenCoverImageUrlByBukken} from "../utils/bukken";
 import {UserGroup} from "../utils/global-data";
 import moment from "moment";
-import { listInformation, queryInformationListSendByUserId , queryInformationListSendByInformationId, getInformation} from "graphql/queries";
+import {
+    listInformation,
+    queryInformationListSendByUserId,
+    queryInformationListSendByInformationId,
+    getInformation,
+} from "graphql/queries";
 import * as mutations from "../graphql/mutations";
 import toast from "react-hot-toast";
 
@@ -22,29 +27,33 @@ export const useInformationList = () => {
     const [loading, setLoading] = useState(false);
     const [page, setPage] = useState(0);
 
-    const getListInformationSendList = useCallback(async (list = [], nextToken = null) => {
-        const response = await API.graphql({
-            query: queryInformationListSendByUserId,
-            variables: {
-                user_id: user.id,
-                nextToken,
-                filter: {
-                    delete_flag: {
-                        eq: 0,
+    const getListInformationSendList = useCallback(
+        async (list = [], nextToken = null) => {
+            const response = await API.graphql({
+                query: queryInformationListSendByUserId,
+                variables: {
+                    user_id: user.id,
+                    nextToken,
+                    filter: {
+                        delete_flag: {
+                            eq: 0,
+                        },
                     },
                 },
-            },
-        });
-        var informationSendList = response.data.queryInformationListSendByUserId.items;
-        list = list.concat(informationSendList);
-        if (response.data.queryInformationListSendByUserId.nextToken) {
-            await getListInformationSendList(
-                list,
-                response.data.queryInformationListSendByUserId.nextToken
-            );
-        }
-        return list;
-    }, [user]);
+            });
+            var informationSendList =
+                response.data.queryInformationListSendByUserId.items;
+            list = list.concat(informationSendList);
+            if (response.data.queryInformationListSendByUserId.nextToken) {
+                await getListInformationSendList(
+                    list,
+                    response.data.queryInformationListSendByUserId.nextToken
+                );
+            }
+            return list;
+        },
+        [user]
+    );
 
     const getListCSInformationSendList = useCallback(
         async (list = [], nextToken = null) => {
@@ -78,15 +87,19 @@ export const useInformationList = () => {
                 query: queryInformationListSendByInformationId,
                 variables: {
                     nextToken,
-                    information_id:  id
+                    information_id: id,
                 },
             });
-            var informationListSend = response.data.queryInformationListSendByInformationId.items;
+            var informationListSend =
+                response.data.queryInformationListSendByInformationId.items;
             list = list.concat(informationListSend);
-            if (response.data.queryInformationListSendByInformationId.nextToken) {
+            if (
+                response.data.queryInformationListSendByInformationId.nextToken
+            ) {
                 await getListInformationSendByInformationId(
                     list,
-                    response.data.queryInformationListSendByInformationId.nextToken,
+                    response.data.queryInformationListSendByInformationId
+                        .nextToken,
                     id
                 );
             }
@@ -95,84 +108,100 @@ export const useInformationList = () => {
         []
     );
 
-    const filterInformationSendList = useCallback(
-         (filter) => {
-          if(filter == "全て"){
-            setInformationList(informationListFirst)
-          }
-          else{
-            const newList = informationListFirst.filter(item => {
-                return item.status == filter
-            })
+    const filterInformationSendList = useCallback((filter) => {
+        if (filter == "全て") {
+            setInformationList(informationListFirst);
+        } else {
+            const newList = informationListFirst.filter((item) => {
+                return item.status == filter;
+            });
             console.log("newList", newList);
-            setInformationList(newList)
-          }
-        },
-        []
-    )
+            setInformationList(newList);
+        }
+    }, []);
 
-    let filterDateInformationSendList =  (start, end, filter) => {
-        setPage(0)
-            if(filter == "全て" ){
-                if(!start || !end){
-                    console.log("ok");
-                    setInformationList(informationListFirst)
+    let filterDateInformationSendList = (start, end, filter) => {
+        setPage(0);
+        if (filter == "全て") {
+            const newList = informationListFirst.filter((item) => {
+                if(start && end){
+                    return (
+                        moment(item.scheduled_delivery_date).valueOf() <
+                            end.getTime() &&
+                        moment(item.scheduled_delivery_date).valueOf() >
+                            start.getTime()
+                    );
+                }
+                else if(!start && end){
+                    return (
+                        moment(item.scheduled_delivery_date).valueOf() <
+                            end.getTime()
+                    );
+                }
+                else if(start && !end){
+                    return (
+                        moment(item.scheduled_delivery_date).valueOf() >
+                        start.getTime()
+                    );
                 }
                 else{
-                    const newList = informationListFirst.filter(item => {
-                        return moment(item.scheduled_delivery_date).valueOf()
-                         < end.getTime() && moment(item.scheduled_delivery_date).valueOf()
-                         > start.getTime()
-                    })
-                    console.log("newList", newList);
-                    setInformationList(newList)
+                    return true
                 }
-            }
-            else{
-                if(!start || !end){
-                    const newList = informationListFirst.filter(item => {
-                        return item.status == filter
-                    })
-                    console.log("newList", newList);
-                    setInformationList(newList)
+               
+            });
+            console.log("newList", newList);
+            setInformationList(newList);
+        } else {
+            const newList = informationListFirst.filter((item) => {
+                if(start && end){
+                    return (
+                        moment(item.scheduled_delivery_date).valueOf() <
+                            end.getTime() &&
+                        moment(item.scheduled_delivery_date).valueOf() >
+                            start.getTime() && item.status == filter
+                    );
+                }
+                else if(!start && end){
+                    return (
+                        moment(item.scheduled_delivery_date).valueOf() <
+                            end.getTime() && item.status == filter
+                    );
+                }
+                else if(start && !end){
+                    return (
+                        moment(item.scheduled_delivery_date).valueOf() >
+                        start.getTime() && item.status == filter
+                    );
                 }
                 else{
-                    const newList = informationListFirst.filter(item => {
-                        return moment(item.scheduled_delivery_date).valueOf()
-                         < end.getTime() && moment(item.scheduled_delivery_date).valueOf()
-                         > start.getTime() && item.status == filter
-                    })
-                    console.log("newList", newList);
-                    setInformationList(newList)
+                    return item.status == filter
                 }
-            }
-          
-       }
+                // return (
+                //     moment(item.scheduled_delivery_date).valueOf() <
+                //         end.getTime() &&
+                //     moment(item.scheduled_delivery_date).valueOf() >
+                //         start.getTime() &&
+                //     item.status == filter
+                // );
+            });
+            console.log("newList", newList);
+            setInformationList(newList);
+        }
+    };
 
-       const deleteInformation = useCallback(
+    const deleteInformation = useCallback(
         async (item) => {
             try {
-                const info =  await API.graphql({
+                const info = await API.graphql({
                     query: getInformation,
                     variables: {
-                      id: item.id
+                        id: item.id,
                     },
                 });
                 // console.log(info);
-                if(info.data?.getInformation?.delete_flag == 0){
-                await API.graphql({
-                    query: mutations.updateInformation,
-                    variables: {
-                        input: {
-                            id: item.id,
-                            delete_flag: 1,
-                        },
-                    },
-                });
-                const listDelete = await getListInformationSendByInformationId([],null,item.id)
-                const res = await Promise.all(listDelete.map(item => {
-                    return API.graphql({
-                        query: mutations.updateInformationListSend,
+                if (info.data?.getInformation?.delete_flag == 0) {
+                    await API.graphql({
+                        query: mutations.updateInformation,
                         variables: {
                             input: {
                                 id: item.id,
@@ -180,19 +209,36 @@ export const useInformationList = () => {
                             },
                         },
                     });
-                }))
-                const newInformationList = R.reject(R.propEq("id", item.id))(
-                    informationList
-                );
-                setInformationList(newInformationList);
-                const newInformationListFirst = R.reject(R.propEq("id", item.id))(
-                    informationListFirst
-                );
-                setInformationListFirst(newInformationListFirst);
-                toast.success('お知らせ情報を削除しました。')
-                }
-                else{
-                    toast.error("該当のお知らせは削除されています。")
+                    const listDelete =
+                        await getListInformationSendByInformationId(
+                            [],
+                            null,
+                            item.id
+                        );
+                    const res = await Promise.all(
+                        listDelete.map((item) => {
+                            return API.graphql({
+                                query: mutations.updateInformationListSend,
+                                variables: {
+                                    input: {
+                                        id: item.id,
+                                        delete_flag: 1,
+                                    },
+                                },
+                            });
+                        })
+                    );
+                    const newInformationList = R.reject(
+                        R.propEq("id", item.id)
+                    )(informationList);
+                    setInformationList(newInformationList);
+                    const newInformationListFirst = R.reject(
+                        R.propEq("id", item.id)
+                    )(informationListFirst);
+                    setInformationListFirst(newInformationListFirst);
+                    toast.success("お知らせ情報を削除しました。");
+                } else {
+                    toast.error("該当のお知らせは削除されています。");
                 }
 
                 //update all documents + history related with this interior
@@ -203,23 +249,21 @@ export const useInformationList = () => {
         [informationList, informationListFirst]
     );
 
-        const updateReadInformation = useCallback(
-            async (id) => {
-                try {
-                    await API.graphql({
-                        query: mutations.updateInformationListSend,
-                        variables: {
-                            input: {
-                                id: id,
-                                last_user_read: moment().utc(),
-                            },
-                        },
-                    });
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-            ,[])
+    const updateReadInformation = useCallback(async (id) => {
+        try {
+            await API.graphql({
+                query: mutations.updateInformationListSend,
+                variables: {
+                    input: {
+                        id: id,
+                        last_user_read: moment().utc(),
+                    },
+                },
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
 
     useEffect(() => {
         async function loadData() {
@@ -229,32 +273,36 @@ export const useInformationList = () => {
             if (user.group === UserGroup.agentGrands) {
                 const list = await getListInformationSendList();
                 console.log(list);
-                informationList = list.filter(item => {
+                informationList = list.filter((item) => {
                     // return ((moment(item.information.scheduled_delivery_date).isBefore(moment().utc(new Date()))) || !item.information.scheduled_delivery_date) && item.information.draft_flag == 0 && item.information.delete_flag == 0
-                    return item.information.processed_date && item.information.draft_flag == 0 && item.information.delete_flag == 0
-                })
+                    return (
+                        item.information.processed_date &&
+                        item.information.draft_flag == 0 &&
+                        item.information.delete_flag == 0
+                    );
+                });
             } else {
-               let list  = await getListCSInformationSendList();
-               informationList = list.map(item => {
-                if(item.draft_flag == 1){
-                    item.status = "下書き"
-                    return item
-                }
-                else if(item.processed_date){
-                    console.log(moment(item.scheduled_delivery_date).valueOf());
-                    console.log(new Date().getTime());
-                    item.status = "送信済"
-                    return item
-                }
-                else{
-                    item.status = "送信予定"
-                    return item
-                }
-               })
+                let list = await getListCSInformationSendList();
+                informationList = list.map((item) => {
+                    if (item.draft_flag == 1) {
+                        item.status = "下書き";
+                        return item;
+                    } else if (item.processed_date) {
+                        console.log(
+                            moment(item.scheduled_delivery_date).valueOf()
+                        );
+                        console.log(new Date().getTime());
+                        item.status = "送信済";
+                        return item;
+                    } else {
+                        item.status = "送信予定";
+                        return item;
+                    }
+                });
             }
-            console.log("DSJHFKJDSFH",informationList);
+            console.log("DSJHFKJDSFH", informationList);
             setInformationList(informationList);
-            setInformationListFirst(informationList)
+            setInformationListFirst(informationList);
             //end load list bukken
             setLoading(false);
         }
@@ -262,5 +310,15 @@ export const useInformationList = () => {
         if (isMounted() && user) loadData();
     }, [isMounted, user]);
 
-    return {informationList, informationListFirst, page, setPage, filterInformationSendList, filterDateInformationSendList, deleteInformation, updateReadInformation, loading};
+    return {
+        informationList,
+        informationListFirst,
+        page,
+        setPage,
+        filterInformationSendList,
+        filterDateInformationSendList,
+        deleteInformation,
+        updateReadInformation,
+        loading,
+    };
 };
